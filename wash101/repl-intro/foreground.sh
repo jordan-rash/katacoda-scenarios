a90fa040f75bef9ab9d4b47d0f1d4ad22dadd7fe
Jordan Rash <cjr@cjrash.com>
2021-03-03 21:46:14 -0700

    Initial Commit

--- wash101/repl-intro/foreground.sh
+++ wash101/repl-intro/foreground.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
-clear
-echo "Initalizing Environment, please wait!"; while [ ! -f /tmp/.init_done ] ; do sleep 2; done;
+echo "Initalizing Environment, please wait!"
+while [ ! -f /tmp/.init_done ]
+    do sleep 2
+done
+
 wash up
