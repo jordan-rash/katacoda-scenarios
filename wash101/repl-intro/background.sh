587d0895fe971d8561c5e540c7654e75f04fea2a
Jordan Rash <cjr@cjrash.com>
2021-03-03 20:19:00 -0700

    Initial Commit

--- wash101/repl-intro/background.sh
+++ wash101/repl-intro/background.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
 curl -s https://packagecloud.io/install/repositories/wasmcloud/core/script.deb.sh | bash
-apt-get update
-apt install wash
 
-WASH_RPC_HOST=demo.nats.io
+docker-compose up -d
 wash up
