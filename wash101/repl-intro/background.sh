0bd5a50470c3ceccaf19defb7b915e9d2f0d4dd1
Jordan Rash <cjr@cjrash.com>
2021-03-03 21:08:28 -0700

    Initial Commit

--- wash101/repl-intro/background.sh
+++ wash101/repl-intro/background.sh
@@ -2,3 +2,5 @@
 curl -s https://packagecloud.io/install/repositories/wasmcloud/core/script.deb.sh | bash
 apt-get install wash
 docker-compose up -d
+
+touch /tmp/.init_done
--- wash101/repl-intro/foreground.sh
+++ wash101/repl-intro/foreground.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
+
+echo "Initalizing Environment, please wait!"; while [ ! -f /tmp/.init_done ] ; do sleep 2; done;
 wash up
